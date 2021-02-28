36828283ca1c1fda0f88204787cbbf6796ac0a67
ALOK SARKAR <33203617+alok123github@users.noreply.github.com>
2021-02-28 20:18:29 +0530

    Update sms.php
    Show html data to php page

--- sms.php
+++ sms.php
@@ -13,12 +13,13 @@ $fdate=$_POST['fdate'];
 $sdate=$_POST['sdate'];
 $fprice=$_POST['fprice'];
 $sprice=$_POST['sprice'];
-$message = urlencode("Hi ".$fname." your first installment price ".$fprice);// urlencode your message
 
-// $message = "Hi "+$fname+" your first installment price "+$fprice+"needs to pay on "+$fdate+" second installment price"+$sprice+"needs to pay on "+$fdate;
+$message = urlencode("Hi ".$fname." your first installment price ".$fprice."needs to pay on ".$fdate." second installment price".$sprice."needs to pay on ".$fdate);
 echo $message;
-curl_setopt($curl, CURLOPT_POST, 1);// set post data to true
-curl_setopt($curl, CURLOPT_POSTFIELDS, "apikey=[NFXIE4XVGTV1XVWH7788JHPB38X0MNNU]&secret=[6EM2IYZOS1Z7V488]&usetype=[stage]&phone=[to-mobile]&senderid=[active-sender-id]&message=[$message]");// post data
+
+
+curl_setopt($curl, CURLOPT_POST, 1);
+curl_setopt($curl, CURLOPT_POSTFIELDS, "apikey=[NFXIE4XVGTV1XVWH7788JHPB38X0MNNU]&secret=[6EM2IYZOS1Z7V488]&usetype=[stage]&phone=[$phone]&senderid=[active-sender-id]&message=[$message]");// post data
 // query parameter values must be given without squarebrackets.
 //  Optional Authentication:
 curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
